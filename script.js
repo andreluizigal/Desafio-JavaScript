293def300b066732a72ddf5eed4cac43b87cde23
anaevanilde <anaevanilde@gmail.com>
2019-09-07 21:06:08 -0300

    repair

--- script.js
+++ script.js
@@ -1,71 +1,56 @@
-let perguntas;
-httpRequest = new XMLHttpRequest();
-
-httpRequest.open('GET', 'https://quiz-trainee.herokuapp.com/questions', true);
-httpRequest.send();
-
-httpRequest.onreadystatechange = function(){
-   if(this.readyState == 4 && this.status == 200){
-      perguntas = (JSON.parse(this.responseText));
-   }
-}
-var perguntaAtual = -1
-var pontos = 0;
+//Nessa variável estão todas as perguntas e respostas que vocês usarão no desafio
+var perguntas = [
+    {
+        texto: 'O céu não é o limite',
+        respostas: [
+            { valor: 0, texto: 'Ter preguiça' },
+            { valor: 1, texto: 'Quando der certo a gente faz' },
+            { valor: 3, texto: 'Vamo pra cima familia' },
+            { valor: 2, texto: 'Se der for viavel a gente faz' }
+        ]
+    },
+    {
+        texto: 'Apaixonados pela missão',
+        respostas: [
+            { valor: 0, texto: 'Projetos nada mais sao do que numeros' },
+            { valor: 1, texto: 'Dinheiro é bom e é isto' },
+            { valor: 3, texto: 'Se meu projeto nao ajudar meu cliente entao nao fiz nada' },
+            { valor: 2, texto: 'Se eu entender o que esta no codigo entao ta show' }
+        ]
+    },
+    {
+        texto: 'Ohana',
+        respostas: [
+            { valor: 0, texto: 'Ta com problema? Se vira filhao' },
+            { valor: 3, texto: 'Voce é meu irmao de batalha, to contigo e nao solto' },
+            { valor: 2, texto: 'Voce é da gti, eu tambem entao tudo certo' },
+            { valor: 1, texto: 'Fora da Gti nem olho' }
+        ]
+    },
+    {
+        texto: 'Integridade',
+        respostas: [
+            { valor: 0, texto: 'Furar a fila do ru' },
+            { valor: 2, texto: 'farol amarelo pra mim é verde' },
+            { valor: 1, texto: 'Clonar cartao de credito' },
+            { valor: 3, texto: 'Sou integro' }
+        ]
+    },
+    {
+        texto: 'Ser capitão da nave',
+        respostas: [
+            { valor: 3, texto: 'Eu serei o lider que eu quero pra mim' },
+            { valor: 0, texto: 'Eu nao sabia, entao nao fiz' },
+            { valor: 1, texto: 'Ve la, depois me conta' },
+            { valor: 2, texto: 'To so esperando meu estagio aprovar pra sair da GTi' }
+        ]
+    }
+]
 
 function mostrarQuestao() {
-    // Esconde e exibe elementos ao iniciar o quiz
-    console.log(perguntas);
-    if (perguntaAtual == -1) {
-        document.getElementById("resultado").style.display = 'none'
-        document.getElementById("listaRespostas").style.display = "inline"
-        document.getElementById("confirmar").innerHTML = "Próxima"
-    } else // Se perguntaAtual > -1 (já começou o quiz)
-        // Verifica se nenhuma resposta foi selecionada
-        for (var i = 0; i < 4; i++)
-            if (document.getElementsByName("resposta")[i].checked) // Se alguma foi selecionada
-                break // Pode ir para a próxima pergunta
-            else if (i == 3) // Se não tem nenhuma selecionada
-                return // Não deixa ir para a próxima
-
-    // Avança para a próxima (ou primeira) pergunta
-    perguntaAtual++
-
-    // Finaliza o quiz se não há mais perguntas
-    if (perguntaAtual >= perguntas.length) {
-        finalizarQuiz();
-        return;
-    }
-
-    // Troca o titulo da pergunta
-    document.getElementById('titulo').innerHTML = perguntas[perguntaAtual].title
-
-    var spansRespostas = document.getElementsByTagName("span")
-    var botoesRespostas = document.getElementsByName("resposta")
-
-    for (var i = 0; i < perguntas[perguntaAtual].options.length; i++) {
-        // Adiciona aos pontos o valor da resposta da pergunta anterior escolhida
-        if (botoesRespostas[i].checked) {
-            pontos += Number(botoesRespostas[i].value)
-            botoesRespostas[i].checked = false // Deseleciona para a próxima pergunta
-        }
-
-        // Atualiza as respostas
-        spansRespostas[i].innerHTML = perguntas[perguntaAtual].options[i].answer
-
-        // Atualiza os valores de cada resposta
-        spansRespostas[i].parentElement.children[0].value = perguntas[perguntaAtual].options[i].value
-
-    }
-
+    
 }
 
 function finalizarQuiz() {
-    var porcentagemFinal = Math.round((pontos / 15) * 100)
-    perguntaAtual = -1
-    pontos = 0
-    document.getElementById('titulo').innerHTML = 'QUIZ DOS VALORES DA GTI'
-    document.getElementById("listaRespostas").style.display = "none"
-    document.getElementById("resultado").style.display = 'block'
-    document.getElementById("resultado").innerHTML = 'Sua pontuação: ' + porcentagemFinal + '%'
-    document.getElementById("confirmar").innerHTML = "Refazer Quiz";
+    
 }
