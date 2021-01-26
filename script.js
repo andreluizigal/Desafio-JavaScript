b686dddcf4ba373264b63cb58f7be4333b65c0f6
André Luiz Ignácio de Almeida Filho <andreluizigal@gmail.com>
2021-01-26 11:51:57 -0300

    Completo

--- script.js
+++ script.js
@@ -1,201 +1,105 @@
-var novo = true;
-var next = false;
-var pontos = 0;
-var q = 0;
+// Variáveis globais
+let pontos = 0;
+var q = -1;
+var questoes;
+var novo;
+
+// Requisitando o conteudo das questões como objeto json
+var requestURL = 'https://quiz-trainee.herokuapp.com/questions';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  // Transferindo o objeto para uma variavel e permitindo iniciar o quiz
+  questoes = request.response;
+  novo = true;
+
+  // Adicionando klayton inicial
+  var klayton = new Image (300, 300);
+  klayton.src = 'images/inicio.png'
+  document.getElementById('resultado').appendChild(klayton);
+}
 
 
-// Função executada ao clicar no botão Começar
 function mostrarQuestao() {
-
-  // Checando se está no meio de um quiz
-  if (novo){
-    
-    // Mudando variavel para dizer que está no meio de um quiz e zerando pontuação
-    novo = false;
-    pontos = 0;
-
-    // Requisitando o conteudo das questões como objeto json
-    var requestURL = 'https://quiz-trainee.herokuapp.com/questions';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-
-    // Executando ao carregar a resposta do servidor
-    request.onload = function() {
-
-      // Transferindo o objeto para uma variavel
-      var questoes = request.response;
-
-      // Fazendo o botao confirmar mudar para próxima
-      document.getElementById("confirmar").innerHTML = 'PRÓXIMA';
-      
-      // Fazendo as questoes aparecerem
-      document.getElementById("listaRespostas").style.display = 'block';
-      
-      // Escrevendo questões
-      var q = 0;
-        // Questão 1
-          // Atribuindo nomes às respostas
-          document.getElementById("titulo").innerHTML = questoes[q].title;
-          document.getElementsByTagName("span")[0].innerHTML = questoes[q].options[0].answer;
-          document.getElementsByTagName("span")[1].innerHTML = questoes[q].options[1].answer;
-          document.getElementsByTagName("span")[2].innerHTML = questoes[q].options[2].answer;
-          document.getElementsByTagName("span")[3].innerHTML = questoes[q].options[3].answer;
-          
-          // Atribuindo valores às respostas
-          document.getElementsByTagName("input")[0].value = questoes[q].options[0].value;
-          document.getElementsByTagName("input")[1].value = questoes[q].options[1].value;
-          document.getElementsByTagName("input")[2].value = questoes[q].options[2].value;
-          document.getElementsByTagName("input")[3].value = questoes[q].options[3].value;
-          
-          console.log("Questão 1");
-          // Mudando de questão caso tenha marcado algum item e pressionado o botao.
-          if(proximaQuestao()){
-            q++;
-
-            // Somando pontuação
-            for (let i = 0; i < 4; i++) {
-              if(document.getElementsByTagName("input")[i].checked){
-                pontos += document.getElementsByTagName("input")[i].value;
-              }
-            }
-            
-            // Questão 2
-            // Atribuindo nomes às respostas
-            document.getElementById("titulo").innerHTML = questoes[q].title;
-            document.getElementsByTagName("span")[0].innerHTML = questoes[q].options[0].answer;
-            document.getElementsByTagName("span")[1].innerHTML = questoes[q].options[1].answer;
-            document.getElementsByTagName("span")[2].innerHTML = questoes[q].options[2].answer;
-            document.getElementsByTagName("span")[3].innerHTML = questoes[q].options[3].answer;
-            
-            // Atribuindo valores às respostas
-            document.getElementsByTagName("input")[0].value = questoes[q].options[0].value;
-            document.getElementsByTagName("input")[1].value = questoes[q].options[1].value;
-            document.getElementsByTagName("input")[2].value = questoes[q].options[2].value;
-            document.getElementsByTagName("input")[3].value = questoes[q].options[3].value;
-
-            console.log("Questão 2");
-            // Mudando de questão caso tenha marcado algum item e pressionado o botao.
-            if(proximaQuestao()){
-              q++;
-
-              // Somando pontuação
-              for (let i = 0; i < 4; i++) {
-                if(document.getElementsByTagName("input")[i].checked){
-                  pontos += document.getElementsByTagName("input")[i].value;
-                }
-              }
-              // Questão 3
-              // Atribuindo nomes às respostas
-              document.getElementById("titulo").innerHTML = questoes[q].title;
-              document.getElementsByTagName("span")[0].innerHTML = questoes[q].options[0].answer;
-              document.getElementsByTagName("span")[1].innerHTML = questoes[q].options[1].answer;
-              document.getElementsByTagName("span")[2].innerHTML = questoes[q].options[2].answer;
-              document.getElementsByTagName("span")[3].innerHTML = questoes[q].options[3].answer;
-              
-              // Atribuindo valores às respostas
-              document.getElementsByTagName("input")[0].value = questoes[q].options[0].value;
-              document.getElementsByTagName("input")[1].value = questoes[q].options[1].value;
-              document.getElementsByTagName("input")[2].value = questoes[q].options[2].value;
-              document.getElementsByTagName("input")[3].value = questoes[q].options[3].value;
-
-              console.log("Questão 3");
-              // Mudando de questão caso tenha marcado algum item e pressionado o botao.
-              if(proximaQuestao()){
-                q++;
-
-                // Somando pontuação
-                for (let i = 0; i < 4; i++) {
-                  if(document.getElementsByTagName("input")[i].checked){
-                    pontos += document.getElementsByTagName("input")[i].value;
-                  }
-                }
-                
-                // Questão 4
-                // Atribuindo nomes às respostas
-                document.getElementById("titulo").innerHTML = questoes[q].title;
-                document.getElementsByTagName("span")[0].innerHTML = questoes[q].options[0].answer;
-                document.getElementsByTagName("span")[1].innerHTML = questoes[q].options[1].answer;
-                document.getElementsByTagName("span")[2].innerHTML = questoes[q].options[2].answer;
-                document.getElementsByTagName("span")[3].innerHTML = questoes[q].options[3].answer;
-                
-                // Atribuindo valores às respostas
-                document.getElementsByTagName("input")[0].value = questoes[q].options[0].value;
-                document.getElementsByTagName("input")[1].value = questoes[q].options[1].value;
-                document.getElementsByTagName("input")[2].value = questoes[q].options[2].value;
-                document.getElementsByTagName("input")[3].value = questoes[q].options[3].value;
-
-                console.log("Questão 4");
-                // Mudando de questão caso tenha marcado algum item e pressionado o botao.
-                if(proximaQuestao()){
-                  q++;
-
-                  // Somando pontuação
-                  for (let i = 0; i < 4; i++) {
-                    if(document.getElementsByTagName("input")[i].checked){
-                      pontos += document.getElementsByTagName("input")[i].value;
-                    }
-                  }
-                  
-                  // Questão 5
-                  // Atribuindo nomes às respostas
-                  document.getElementById("titulo").innerHTML = questoes[q].title;
-                  document.getElementsByTagName("span")[0].innerHTML = questoes[q].options[0].answer;
-                  document.getElementsByTagName("span")[1].innerHTML = questoes[q].options[1].answer;
-                  document.getElementsByTagName("span")[2].innerHTML = questoes[q].options[2].answer;
-                  document.getElementsByTagName("span")[3].innerHTML = questoes[q].options[3].answer;
-                  
-                  // Atribuindo valores às respostas
-                  document.getElementsByTagName("input")[0].value = questoes[q].options[0].value;
-                  document.getElementsByTagName("input")[1].value = questoes[q].options[1].value;
-                  document.getElementsByTagName("input")[2].value = questoes[q].options[2].value;
-                  document.getElementsByTagName("input")[3].value = questoes[q].options[3].value;
-                  
-                  console.log("Questão 5");
-                  // Mudando de questão caso tenha marcado algum item e pressionado o botao.
-                  if(proximaQuestao()){
-                    // Somando pontuação
-                    for (let i = 0; i < 4; i++) {
-                      if(document.getElementsByTagName("input")[i].checked){
-                        pontos += document.getElementsByTagName("input")[i].value;
-                      }
-                    }
-                    
-                    // Finalizando quiz
-                    finalizarQuiz(pontos);
-                  }
-                }
-              }
-            }
-          }
-      finalizarQuiz(pontos);    
+    // Atribuindo nomes e valores da questão
+    document.getElementById("titulo").innerHTML = questoes[q].title;
+    for (let i = 0; i < questoes[q].options.length; i++) {
+        document.getElementsByTagName("span")[i].innerHTML = questoes[q].options[i].answer;
+        document.getElementsByTagName("input")[i].value = questoes[q].options[i].value;
     }
-  }
 }
 
 // Checa se é possivel avançar para proxima questão
 function proximaQuestao(){
-  if(novo){
-    var marcado = false;
-    for (let m = 0; m < 4; m++) {
-      if(document.getElementsByTagName("input")[m].checked){
-        marcado = true;
-        console.log('marcado');
-      }
+    if(novo){
+        // Fazendo o botao confirmar mudar para próxima
+        document.getElementById("confirmar").innerHTML = 'PRÓXIMA';
+      
+        // Fazendo as questoes aparecerem
+        document.getElementById("listaRespostas").style.display = 'block';
+
+        // Zerando pontos, mudando status do quiz e avançando para as questões
+        pontos = 0;
+        document.getElementById("resultado").innerHTML = " ";
+        novo = false;
+        q++;
+        mostrarQuestao();
     }
-  }
+    if(novo != true){
+        // Checando se marcou resposta, somando pontos e avançando para próxima
+        for (let m = 0; m < questoes[q].options.length; m++) {
+          if(document.getElementsByTagName("input")[m].checked){
+            pontos += 1*document.getElementsByTagName("input")[m].value;
+            document.getElementsByTagName("input")[m].checked = false;
+            q++;
+            console.log("pontuação atual: " + pontos + "/15"); // Pontuação atual no prompt
+            if(q < questoes.length){
+                mostrarQuestao();
+            }
+
+            // Encerrando o quiz caso chegue à última questão
+            else{
+                finalizarQuiz();
+                return null;
+            }
+          }
+        }
+    }
 }
 
 
-function finalizarQuiz(p) {
-  // Exibindo pontuação
-  document.getElementById("resultado").innerHTML = "Sua pontuação: " + 100*p/15 + "%";
-  
-  // Fazendo o botao 'próxima' mudar para 'Refazer quiz'
-  document.getElementById("confirmar").innerHTML = 'Refazer quiz';
-  
-  // Mudando status de quiz em andamento
-  novo = true;
+function finalizarQuiz() {
+    // Mudando título
+    document.getElementById("titulo").innerHTML = "QUIZ DOS VALORES DA GTI"
+
+    // Fazendo as questoes desaparecerem
+    document.getElementById("listaRespostas").style.display = 'none';
+    
+    // Exibindo pontuação e klayton correspondente
+    var klayton = new Image (300, 300);
+    if(pontos > 13){
+      klayton.src = 'images/bom.png'
+    }
+    else if(pontos > 8){
+      klayton.src = 'images/ok.png'
+    }
+    else if(pontos > 4){
+      klayton.src = 'images/medio.png'
+    }
+    else{
+      klayton.src = 'images/ruim.png'
+    }
+    document.getElementById('resultado').appendChild(klayton);
+    document.getElementById("resultado").innerHTML += "<br></br>" + "Sua pontuação: " + 100*pontos/15 + "%";
+
+
+    // Fazendo o botao 'próxima' mudar para 'Refazer quiz'
+    document.getElementById("confirmar").innerHTML = 'Refazer quiz';
+    
+    // Mudando status do quiz e resetando questões
+    q = -1;
+    novo = true;
 }
 
